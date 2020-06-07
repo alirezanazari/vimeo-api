f4ec809bad4c8206db38c9da9b594b88782480ed
Alireza <a.alirezanazari@gmail.com>
2020-06-07 16:24:52 +0430

    Implement MainActivity and application class

---
+++ app/src/main/java/ir/alirezanazari/vimeoapi/G.kt
@@ -0,0 +1,11 @@
+package ir.alirezanazari.vimeoapi
+
+import android.app.Application
+
+
+class G : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
---
+++ app/src/main/java/ir/alirezanazari/vimeoapi/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package ir.alirezanazari.vimeoapi.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ir.alirezanazari.vimeoapi.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
