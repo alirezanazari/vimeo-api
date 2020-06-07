5a243290316bba05665714bb4ea751eeb66ae36b
Alireza <a.alirezanazari@gmail.com>
2020-06-07 16:34:22 +0430

    Implement fragments navigator

---
+++ app/src/main/java/ir/alirezanazari/vimeoapi/internal/Navigator.kt
@@ -0,0 +1,40 @@
+package ir.alirezanazari.vimeoapi.internal
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import ir.alirezanazari.vimeoapi.R
+import ir.alirezanazari.vimeoapi.ui.search.SearchFragment
+import ir.alirezanazari.vimeoapi.ui.video.VideoFragment
+
+
+class Navigator {
+
+    companion object {
+
+        fun openSearch(fm: FragmentManager) {
+            load(true, fm, SearchFragment())
+        }
+
+        fun openVideo(id: String, name: String, fm: FragmentManager?) {
+            load(false, fm, VideoFragment.newInstance())
+        }
+
+        private fun load(isReplace: Boolean, fm: FragmentManager?, fragment: Fragment) {
+            if (isReplace) {
+                fm?.let {
+                    it.beginTransaction()
+                        .addToBackStack(fragment.javaClass.name)
+                        .replace(R.id.fragments_container, fragment, fragment.javaClass.name)
+                        .commit()
+                }
+            } else {
+                fm?.let {
+                    it.beginTransaction()
+                        .addToBackStack(fragment.javaClass.name)
+                        .add(R.id.fragments_container, fragment, fragment.javaClass.name)
+                        .commit()
+                }
+            }
+        }
+    }
+}
--- app/src/main/java/ir/alirezanazari/vimeoapi/ui/MainActivity.kt
+++ app/src/main/java/ir/alirezanazari/vimeoapi/ui/MainActivity.kt
@@ -3,11 +3,13 @@ package ir.alirezanazari.vimeoapi.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import ir.alirezanazari.vimeoapi.R
+import ir.alirezanazari.vimeoapi.internal.Navigator
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Navigator.openSearch(supportFragmentManager)
     }
 }
