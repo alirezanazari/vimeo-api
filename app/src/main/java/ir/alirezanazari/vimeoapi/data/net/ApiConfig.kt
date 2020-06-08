4fc6518ba24d1cfd522d03e251948f5a4d28718f
Alireza <a.alirezanazari@gmail.com>
2020-06-08 18:49:10 +0430

    Add Okhttp request logger interceptor

--- app/src/main/java/ir/alirezanazari/vimeoapi/data/net/ApiConfig.kt
+++ app/src/main/java/ir/alirezanazari/vimeoapi/data/net/ApiConfig.kt
@@ -1,8 +1,10 @@
 package ir.alirezanazari.vimeoapi.data.net
 
 import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
+import ir.alirezanazari.vimeoapi.BuildConfig
 import ir.alirezanazari.vimeoapi.internal.Constants
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
@@ -19,10 +21,15 @@ class ApiConfig {
             val okHttpClient = OkHttpClient.Builder()
                 .addInterceptor(tokenInterceptor)
                 .connectTimeout(16, TimeUnit.SECONDS)
-                .build()
+
+            if (BuildConfig.DEBUG) {
+                val httpLoggingInterceptor = HttpLoggingInterceptor()
+                httpLoggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+                okHttpClient.addInterceptor(httpLoggingInterceptor)
+            }
 
             return Retrofit.Builder()
-                .client(okHttpClient)
+                .client(okHttpClient.build())
                 .baseUrl(Constants.Net.BASE_URL)
                 .addCallAdapterFactory(CoroutineCallAdapterFactory())
                 .addConverterFactory(GsonConverterFactory.create())
