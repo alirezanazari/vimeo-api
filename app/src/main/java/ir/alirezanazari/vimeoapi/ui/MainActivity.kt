1c6f6c11ffbd60fae5b8509f92418c0c223e5fb9
Alireza <a.alirezanazari@gmail.com>
2020-06-09 15:03:40 +0430

    Fix MainActivity back press

--- app/src/main/java/ir/alirezanazari/vimeoapi/ui/MainActivity.kt
+++ app/src/main/java/ir/alirezanazari/vimeoapi/ui/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import ir.alirezanazari.vimeoapi.R
 import ir.alirezanazari.vimeoapi.internal.Navigator
+import ir.alirezanazari.vimeoapi.ui.search.SearchFragment
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,5 +12,19 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Navigator.openSearch(supportFragmentManager)
+    }
+
+    override fun onBackPressed() {
+        if(supportFragmentManager.backStackEntryCount == 1){
+            val fragment = supportFragmentManager.findFragmentById(R.id.fragments_container)
+            if(fragment is SearchFragment){
+                if(fragment.onBackPressed()) finish()
+                return
+            }else {
+                finish()
+                return
+            }
+        }
+        super.onBackPressed()
     }
 }
