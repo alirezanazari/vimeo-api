2b5d117cc6d8c1527f145c79701046b092762da0
Alireza <a.alirezanazari@gmail.com>
2020-06-09 14:05:25 +0430

    Improve play video with Exo player

--- app/src/main/java/ir/alirezanazari/vimeoapi/ui/video/VideoFragment.kt
+++ app/src/main/java/ir/alirezanazari/vimeoapi/ui/video/VideoFragment.kt
@@ -1,5 +1,6 @@
 package ir.alirezanazari.vimeoapi.ui.video
 
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,6 +8,11 @@ import android.view.ViewGroup
 import androidx.core.os.bundleOf
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.exoplayer2.DefaultLoadControl
+import com.google.android.exoplayer2.SimpleExoPlayer
+import com.google.android.exoplayer2.source.ProgressiveMediaSource
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector
+import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory
 import ir.alirezanazari.vimeoapi.R
 import ir.alirezanazari.vimeoapi.internal.Logger
 import ir.alirezanazari.vimeoapi.ui.BaseFragment
@@ -25,6 +31,7 @@ class VideoFragment : BaseFragment() {
     private val picturesAdapter: VideoPicturesAdapter by inject()
     private val commentsAdapter: VideoCommentsAdapter by inject()
     private lateinit var uri: String
+    private var player: SimpleExoPlayer? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -81,9 +88,9 @@ class VideoFragment : BaseFragment() {
             }
         })
 
-        viewModel.videoMP4Response.observe(viewLifecycleOwner , Observer {
+        viewModel.videoMP4Response.observe(viewLifecycleOwner, Observer {
             it?.let {
-                Logger.showLog(it)
+                prepareVideoPlayer(it)
             }
         })
 
@@ -112,6 +119,32 @@ class VideoFragment : BaseFragment() {
                 }
             }
         })
+    }
+
+    private fun prepareVideoPlayer(url: String) {
+
+        player = SimpleExoPlayer.Builder(videoPlayer.context)
+            .setTrackSelector(DefaultTrackSelector(videoPlayer.context))
+            .setLoadControl(DefaultLoadControl())
+            .build()
+
+        player?.playWhenReady = true
+        videoPlayer.player = player
+
+        val mediaSource = ProgressiveMediaSource.Factory(DefaultHttpDataSourceFactory("VimeoApi"))
+            .createMediaSource(Uri.parse(url))
+
+        player?.prepare(mediaSource, true, false)
+
+    }
+
+    override fun onStop() {
+        super.onStop()
+        player?.let {
+            it.playWhenReady = false
+            it.release()
+        }
+        player = null
     }
 
     override fun onBackPressed(): Boolean = true
